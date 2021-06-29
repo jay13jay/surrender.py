fb0f3bc437bee434c0e2378d902a9139162731c8
Josh Hackney <josh.hackney13@gmail.com>
2021-06-29 02:31:00 -0400

    renamed config file, added a render.py to start abstracting all of the functions into a class, ostensibly to allow multiple sessions, but also for readability

---
+++ render.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+
+# 
+if __name__ == "__main__":
+    print('''
+    This object is not meant to be called directly
+    Please run the program by executing surrender.py
+    ''')
